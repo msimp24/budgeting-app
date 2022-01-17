d0ad8ba19519bd3f13a5a5b3873fa5ac99f2ddd9
Mark Simpson <mark.simpson4@gmail.com>
2022-01-16 22:08:56 -0800

    Budget app completed

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var totalBudget = 0;
 var totalExpense = 0;
 var totalBalance = 0;
-
+var id = 0;
 
 
 // green widget input and button DOM assignment
@@ -17,8 +17,7 @@ const budget = document.querySelector("#budget");
 const expense = document.querySelector("#expense");
 const balance = document.querySelector("#balance");
 
-const expDataTitle = document.querySelector("#title");
-const expDataAmt = document.querySelector("#amount");
+const dataTable = document.querySelector(".data-table");
 
 calcBtn.addEventListener('click', () =>{
     budgetCalc();
@@ -31,15 +30,15 @@ expenseBtn.addEventListener('click', () =>{
 
 function budgetCalc(){
     totalBudget = budgetInput.value;
+
     budgetInput.style.borderColor = 'lightgray'
     
-    if(isNumber(totalBudget)){
+    if(isNumber(totalBudget) && totalBudget != 0){
         budget.classList.add("green");
         budget.textContent = "$ " + budgetInput.value;
         totalBalance = totalBudget - totalExpense;
         balance.textContent = "$ " + totalBalance;
         if(totalBalance > 0){
-            console.log(totalBalance);
             balance.setAttribute('class', 'green');
     
         }
@@ -62,31 +61,37 @@ function budgetCalc(){
 
 function expenseCalc(){
     var expenseType = expenseTypeInput.value;
-    totalExpense = expenseAmtInput.value;
+    var currExpense = 0;
+    var currExpense = parseInt(expenseAmtInput.value);
+    console.log(currExpense);
     if(expenseType === ""){
         expenseType = "Undefined";
     }
     expenseTypeInput.style.borderColor = 'lightgray';
     expenseAmtInput.style.borderColor = 'lightgray';
 
-    if(isNumber(totalExpense)){
+    if(isNumber(currExpense)){
 
+        expense.setAttribute('class', 'red');
+        totalExpense  = totalExpense + currExpense;
+ 
         expense.textContent = "$" + totalExpense;
         totalBalance = totalBudget - totalExpense;
         balance.textContent = "$ " + totalBalance;
 
+
         var newExpense = document.createElement('h4'); 
-        newExpense.innerText = "- " + expenseType;
+        var caps1 = expenseType.toUpperCase();
+        newExpense.innerText = "- " + caps1;
         newExpense.classList.add('red');
-        expDataTitle.append(newExpense);
-
+        
         var expAmount = document.createElement('h4');
-        expAmount.innerText  = "- " + totalExpense;
+        expAmount.innerText  = "- " + currExpense;
         expAmount.classList.add("red");
-        expDataAmt.append(expAmount);
 
 
-        expense.classList.add("red");
+        addRow(newExpense, expAmount);
+
 
         if(totalBalance > 0){
             balance.setAttribute('class', 'green');
@@ -101,6 +106,8 @@ function expenseCalc(){
         expenseAmtInput.style.borderColor = "red";
         return;
     }
+    expenseTypeInput.value = "";
+    expenseAmtInput.value = "";
 
 }
 
@@ -111,5 +118,98 @@ function isNumber(value){
     else{
         return true;
     }
+}
+
+//need to create table wi
+
+function addRow(expTitle, expAmount){
+    var editImg = document.createElement('img');
+    var delImg = document.createElement('img');
+    editImg.src = "/images/edit.png";
+    delImg.src = "/images/delete.png";  
+    delImg.setAttribute('id', 'icon-buttons');
+    editImg.setAttribute('id', 'icon-buttons');
+    editImg.setAttribute('onclick', 'editExpense(this)');
+    delImg.setAttribute('onclick', 'removeRow(this)');
+
+    var rowCount = dataTable.rows.length;
+    var tr = dataTable.insertRow(rowCount);
+
+    for(let i = 0; i<3; i++){
+        var td = document.createElement("td");
+        td = tr.insertCell(i);
+
+        if(i==0){
+            td.append(expTitle);
+        }
+        if(i==1){
+            td.append(expAmount);
+        }
+        if(i==2){
+            td.append(editImg, delImg)
+        }
+    }
+
+    
+    
+}
+
+function removeRow(e){
+    var rowIndex = e.parentNode.parentNode.rowIndex;
+
+    var removeExpense = parseInt(dataTable.rows[rowIndex].cells[1].innerText.substr(2));
+    totalBalance += removeExpense;
+    totalExpense -= removeExpense;
+
+    if(totalBalance > 0){
+        balance.setAttribute('class', 'green');
+    }
+    if(totalBalance < 0){
+        balance.setAttribute('class', 'red');
+    }
+    if(totalBalance == 0){
+        balance.setAttribute('class', 'black');
+    }
+    if(totalExpense == 0){
+        expense.setAttribute('class', 'black');
+    }
+
+    balance.textContent = "$ " + totalBalance;
+    expense.textContent = "$ " + totalExpense;
+
+
+    dataTable.deleteRow(e.parentNode.parentNode.rowIndex);
+
+}
+function editExpense(e){
+    var rowIndex = e.parentNode.parentNode.rowIndex;
+
+    var removeExpense = parseInt(dataTable.rows[rowIndex].cells[1].innerText.substr(2));
+    totalBalance += removeExpense;
+    totalExpense -= removeExpense;
+
+    if(totalBalance > 0){
+        balance.setAttribute('class', 'green');
+    }
+    if(totalBalance < 0){
+        balance.setAttribute('class', 'red');
+    }
+    if(totalBalance == 0){
+        balance.setAttribute('class', 'black');
+    }
+    if(totalExpense == 0){
+        expense.setAttribute('class', 'black');
+    }
+
+
+    balance.textContent = "$ " + totalBalance;
+    expense.textContent = "$ " + totalExpense;
+
+    expenseTypeInput.focus();
+
+    dataTable.deleteRow(e.parentNode.parentNode.rowIndex);
+
+
+
 }
 
